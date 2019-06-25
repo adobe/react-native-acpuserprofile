83ef9b0596f3f81dfbe8124465eb52ff11158e15
Nick Porter <nporter@adobe.com>
2019-06-25 10:12:57 -0600

    Add userprofile to sample app

--- sample/ACPUserProfileSample/android/app/src/main/java/com/acpuserprofilesample/MainApplication.java
+++ sample/ACPUserProfileSample/android/app/src/main/java/com/acpuserprofilesample/MainApplication.java
@@ -14,6 +14,7 @@ package com.acpuserprofilesample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.adobe.marketing.mobile.reactnative.userprofile.RCTACPUserProfilePackage;
 import com.adobe.marketing.mobile.reactnative.RCTACPCorePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTACPUserProfilePackage(),
             new RCTACPCorePackage()
       );
     }
